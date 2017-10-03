8cddf085d598dd9482c7b74bfbdd59256f6ba3da
hkb <hkb@tisynch.com>
2017-10-03 14:44:33 +0200

    findByProductCode returns a Product list

--- src/main/java/com/hkb/shoppingcart/repo/ProductRepository.java
+++ src/main/java/com/hkb/shoppingcart/repo/ProductRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 
 public interface ProductRepository extends MongoRepository<Product, String> {
-    public Product findByProductCode(String productCode);
+    public List<Product> findByProductCode(String productCode);
     public List<Product> findByProductName(String productName);
     public List<Product> findAll();
 }
