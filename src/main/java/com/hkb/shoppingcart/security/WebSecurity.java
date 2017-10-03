c8f405f8018b939496558eebe9524fa5474b2799
hkb <hkb@tisynch.com>
2017-10-03 14:47:13 +0200

    excluding carts endpoint from security for some manual tests

--- src/main/java/com/hkb/shoppingcart/security/WebSecurity.java
+++ src/main/java/com/hkb/shoppingcart/security/WebSecurity.java
@@ -28,6 +28,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
+                .antMatchers(HttpMethod.POST, "/carts").permitAll()
+                .antMatchers(HttpMethod.GET, "/carts").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
