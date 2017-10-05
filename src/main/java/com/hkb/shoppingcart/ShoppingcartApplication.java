cba5f8b5b54ff42cb871f268a029842f60f04b6e
hkb <hkb@tisynch.com>
2017-10-05 12:57:32 +0200

    init application with products inside

--- src/main/java/com/hkb/shoppingcart/ShoppingcartApplication.java
+++ src/main/java/com/hkb/shoppingcart/ShoppingcartApplication.java
@@ -1,9 +1,12 @@
 package com.hkb.shoppingcart;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import com.hkb.shoppingcart.model.CartUser;
+import com.hkb.shoppingcart.model.Product;
 import com.hkb.shoppingcart.repo.CartUserRepository;
+import com.hkb.shoppingcart.repo.ProductRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +22,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class ShoppingcartApplication {
 
 	@Autowired
-	private CartUserRepository repo;
+	private CartUserRepository cartUserRepository;
+
+	@Autowired
+	private ProductRepository productRepository;
 
 	private static final Logger logger = LoggerFactory.getLogger(ShoppingcartApplication.class);
 
@@ -46,16 +52,72 @@ public class ShoppingcartApplication {
 							System.out.println(beanName);
 					}
 
-					repo.deleteAll();
+					cartUserRepository.deleteAll();
+					productRepository.deleteAll();
 
 					// save a couple of users
-					repo.save(new CartUser("Alice", "Smith", "alice", bCryptPasswordEncoder.encode("toto"), "alice@gmail.com"));
-					repo.save(new CartUser("Bob", "Smith", "bob", bCryptPasswordEncoder.encode("toto"), "bob@gmail.com"));
-					repo.save(new CartUser("toto", "toto", "toto", bCryptPasswordEncoder.encode("toto"), "toto@gmail.com"));
+					cartUserRepository.save(new CartUser("Alice", "Smith", "alice", bCryptPasswordEncoder.encode("toto"), "alice@gmail.com"));
+					cartUserRepository.save(new CartUser("Bob", "Smith", "bob", bCryptPasswordEncoder.encode("toto"), "bob@gmail.com"));
+					cartUserRepository.save(new CartUser("toto", "toto", "toto", bCryptPasswordEncoder.encode("toto"), "toto@gmail.com"));
+
+					//Initialize the default products
+					SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+
+
+					productRepository.save(
+							new Product(
+									"Leaf Rake",
+									"GDN-0011",
+									19.95f,
+									ft.parse("2017-03-19T15:15:55.570Z"),
+									"Leaf rake with 48-inch wooden handle.",
+									3.2f,
+									"http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+							));
+					productRepository.save(
+							new Product(
+									"Garden Cart",
+									"GDN-0023",
+									32.95f,
+									ft.parse("2017-03-18T08:15:55.570Z"),
+									"15 gallon capacity rolling garden cart",
+									4.2f,
+									"http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+							));
+					productRepository.save(
+							new Product(
+									"Hammer",
+									"TBX-0048",
+									8.9f,
+									ft.parse("2017-05-21T12:15:55.570Z"),
+									"Curved claw steel hammer",
+									4.8f,
+									"http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+							));
+					productRepository.save(
+							new Product(
+									"Saw",
+									"TBX-0022",
+									11.55f,
+									ft.parse("2017-05-15T12:15:55.570Z"),
+									"15-inch steel blade hand saw",
+									3.7f,
+									"http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
+							));
+					productRepository.save(
+							new Product(
+									"Video Game Controller",
+									"GMG-0042",
+									35.95f,
+									ft.parse("2017-10-15T14:15:55.570Z"),
+									"15-inch steel blade hand saw",
+									4.6f,
+									"http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
+							));
 					// fetch all users
 					System.out.println("Users found with findAll():");
 					System.out.println("-------------------------------");
-					for (CartUser cartUser : repo.findAll()) {
+					for (CartUser cartUser : cartUserRepository.findAll()) {
 						System.out.println(cartUser);
 					}
 					System.out.println();
@@ -63,11 +125,11 @@ public class ShoppingcartApplication {
 					// fetch an individual user
 					System.out.println("CartUser found with findByFirstName('Alice'):");
 					System.out.println("--------------------------------");
-					System.out.println(repo.findByFirstName("Alice"));
+					System.out.println(cartUserRepository.findByFirstName("Alice"));
 
 					System.out.println("Users found with findByLastName('Smith'):");
 					System.out.println("--------------------------------");
-					for (CartUser cartUser : repo.findByLastName("Smith")) {
+					for (CartUser cartUser : cartUserRepository.findByLastName("Smith")) {
 						System.out.println(cartUser);
 					}
 
