2e03ba0faa66bab9dc577c938e3574efc38b5de2
hkb <hkb@tisynch.com>
2017-10-06 12:20:37 +0200

    updated new exception

--- src/main/java/com/hkb/shoppingcart/exceptions/ShoppingCartListNotFoundException.java
+++ src/main/java/com/hkb/shoppingcart/exceptions/ShoppingCartListNotFoundException.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ShoppingCartListNotFoundException extends RuntimeException {
+    public ShoppingCartListNotFoundException() {
+        super("Could not find any shopping cart.");
+    }
     public ShoppingCartListNotFoundException(String userName) {
         super("Could not find shopping cart list for user'" + userName + "'.");
     }
