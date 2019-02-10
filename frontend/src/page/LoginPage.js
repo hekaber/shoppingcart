46b1320e4c373a2f0fe9cae0d4e2774760a6d9ea
sankita15 <ankitacs0018@gmail.com>
2019-02-10 14:59:09 +0530

    Login functionality with input field validation

--- frontend/src/page/LoginPage.js
+++ frontend/src/page/LoginPage.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import {Input, Label, Col, Container, FormGroup, Form, Button} from 'reactstrap';
+import {Input, Label, Col, Container, FormGroup, Form, Button, FormFeedback} from 'reactstrap';
 
 export default class LoginPage extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            email: false,
-            password: false
+            username: '',
+            password: ''
         }
     }
 
     validate(fieldName, fieldValue) {
-        this.setState({
-            [fieldName]: fieldValue.length > 0
-        })
+        const usernameRex = /^[a-zA-Z0-9]{3,10}$/;
+        const passwordRex = /^[a-zA-Z0-9!@#$%^&*]{4,8}$/;
+        const regex = fieldName === 'email' ? usernameRex : passwordRex;
+            fieldValue.length > 0 && this.handleFieldChange(fieldName,fieldValue,regex);
     }
 
+    handleFieldChange(fieldName,fieldValue,regex){
+       this.setState({
+           [fieldName]: regex.test(fieldValue)
+       })
+    }
+
+
     canSubmit() {
-        const {email, password} = this.state;
-        return email && password;
+        const {username, password} = this.state;
+        return username && password;
     }
 
     render() {
@@ -31,13 +39,15 @@ export default class LoginPage extends React.Component {
                             <FormGroup>
                                 <h2>Login Form</h2>
                                 <div className="form-group">
-                                    <Label>Email</Label>
-                                    <Input type="email"
+                                    <Label>Username</Label>
+                                    <Input type="username"
                                            name="username"
                                            className="login-form__textfield"
-                                           placeholder="example@example.com"
-                                           onChange={(e) => this.validate('email', e.target.value)}
+                                           placeholder="Username"
+                                           onBlur={(e) => this.validate('username', e.target.value)}
+                                           invalid={!this.state.username && this.state.username!==''}
                                     />
+                                    <FormFeedback>Snap! your username is not valid</FormFeedback>
                                     <small id="emailHelp" className="form-text text-muted">We'll never share your email
                                         with anyone else.
                                     </small>
@@ -52,8 +62,10 @@ export default class LoginPage extends React.Component {
                                            name="password"
                                            className="login-form__textfield"
                                            placeholder="*******"
-                                           onChange={(e) => this.validate('password', e.target.value)}
+                                           onBlur={(e) => this.validate('password', e.target.value)}
+                                           invalid={!this.state.password && this.state.password!==''}
                                     />
+                                    <FormFeedback>Password must be more than 4 characters & less than 8 characters</FormFeedback>
                                 </div>
                             </FormGroup>
                         </Col>
