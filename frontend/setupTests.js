5ab895357f0f68c32795beace9efd8789605a039
sankita15 <ankitacs0018@gmail.com>
2019-02-12 14:21:59 +0530

    Add in test setup file for flush promises

--- frontend/setupTests.js
+++ frontend/setupTests.js
@@ -2,3 +2,7 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
+
+global.fetch = require('jest-fetch-mock');
+
+global.flushPromises= () => new Promise( resolve => setImmediate(resolve));
